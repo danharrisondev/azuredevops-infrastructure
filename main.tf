3f45a32e4f19ac05dba59eac60530c06d3c78c46
Daniel Harrison <harrison92d@gmail.com>
2022-06-16 18:46:15 +0100

    Add main.tf

---
+++ main.tf
@@ -0,0 +1,31 @@
+variable "org_service_url" {
+  type = string
+}
+
+variable "personal_access_token" {
+    type = string
+}
+
+terraform {
+  required_providers {
+    azuredevops = {
+        source = "microsoft/azuredevops"
+        version = ">=0.1.0"
+    }
+
+    azurerm = {
+        source = "hashicorp/azurerm"
+        version = ">=3.10.0"
+    }
+  }
+}
+
+provider "azuredevops" {
+  org_service_url = var.org_service_url
+  personal_access_token = var.personal_access_token
+}
+
+resource "azuredevops_project" "first_project" {
+  name = "First Project"
+  description = "Constructed with Terraform"
+}
